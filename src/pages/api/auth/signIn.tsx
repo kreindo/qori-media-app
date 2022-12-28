b2b35c621da4cc6e6af815f24abe72c81b218c09
ahmadsan <ahmadsan4p@gmail.com>
2022-12-28 10:13:36 +0800

    sign in page

--- src/pages/api/auth/signIn.tsx
+++ src/pages/api/auth/signIn.tsx
@@ -1,61 +1,22 @@
-import { signIn, getCsrfToken, getProviders } from "next-auth/react";
-import Image from "next/image";
-import Header from "../../components/header";
-import styles from "../../styles/Signin.module.css";
+import { getProviders, signIn } from "next-auth/react";
 
-const Signin = ({ csrfToken, providers }) => {
+export default function SignIn({ providers }: { providers: object }) {
   return (
-    <div style={{ overflow: "hidden", position: "relative" }}>
-      <Header />
-      <div className={styles.wrapper} />
-      <div className={styles.content}>
-        <div className={styles.cardWrapper}>
-          <Image
-            src="/katalog_full.svg"
-            width="196px"
-            height="64px"
-            alt="App Logo"
-            style={{ height: "85px", marginBottom: "20px" }}
-          />
-          <div className={styles.cardContent}>
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <input
-              placeholder="Email (Not Setup - Please Use Github)"
-              size="large"
-            />
-            <button className={styles.primaryBtn}>Submit</button>
-            <hr />
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <div key={provider.name} style={{ marginBottom: 0 }}>
-                  <button onClick={() => signIn(provider.id)}>
-                    Sign in with {provider.name}
-                  </button>
-                </div>
-              ))}
-          </div>
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
         </div>
-      </div>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/login_pattern.svg"
-        alt="Pattern Background"
-        layout="fill"
-        className={styles.styledPattern}
-      />
-    </div>
+      ))}
+    </>
   );
-};
+}
 
-export default Signin;
-
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   const providers = await getProviders();
-  const csrfToken = await getCsrfToken(context);
   return {
-    props: {
-      providers,
-      csrfToken,
-    },
+    props: { providers },
   };
 }
