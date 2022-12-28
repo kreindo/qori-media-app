848904b82ad879686043763063463ff4e779dc9e
ahmadsan <ahmadsan4p@gmail.com>
2022-12-28 10:29:31 +0800

    user page

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
+import { BsThreeDots } from "react-icons/bs";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Image from "next/image";
 const User = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -10,6 +12,31 @@ const User = () => {
         <Link href={"/"}>👈</Link>
       </button>
       <h1>Hello there, {id}</h1>
+      <div className="overflow-hidden p-5">
+        <div className="flex p-4">
+          {/* logo */}
+          <div className="relative rounded-full ">
+            <Image
+              src={"github.com/kreindo.png"}
+              alt={"user profile photo"}
+              fill
+            />
+          </div>
+          {/* name */}
+          <p>{id}</p>
+          {/* follow button with 3 dots */}
+          <div className="flex">
+            <button className="bg-[#AB44FC] px-4 py-2 text-white">
+              Follow
+            </button>
+            <button>
+              <BsThreeDots />
+            </button>
+          </div>
+        </div>
+        <div>{/* bio */}</div>
+        <div>{/* stats */}</div>
+      </div>
     </Layout>
   );
 };
